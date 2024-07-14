02020a8a35eb58be67c6b5d85ffb59e5c208c1dc
ajayrajaloor <ajayrajaloor@gmail.com>
2024-07-15 00:37:23 +0530

    created the restaurant creating form and also validated with zod includes details,cuisines,menuItems and image

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { User } from "@/types";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod/src/zod.js";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
