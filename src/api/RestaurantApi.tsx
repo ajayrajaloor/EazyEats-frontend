c528007e599d875a5d5d65d47084f2b29b82c86c
ajayrajaloor <ajayrajaloor@gmail.com>
2024-08-05 12:00:23 +0530

    added search filter,pagination,cuisines filter and sort options filter

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -1,21 +1,31 @@
+import { SearchState } from "@/pages/SearchPage";
 import { RestaurantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useSearchRestaurants = (city?: string) =>{
+export const useSearchRestaurants = (searchState:SearchState, city?: string) =>{
+    
     const createSearchRequest = async(): Promise<RestaurantSearchResponse> =>{
-        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}`);
+
+        const params = new URLSearchParams();
+        
+        params.set("searchQuery", searchState.searchQuery)
+        params.set("page", searchState.page.toString());
+        params.set("selectedCuisines", searchState.selectedCuisines.join(","))
+        params.set("sortOption", searchState.sortOption)
+        
+        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`);
 
         if(!response.ok){
             throw new Error("Failed to get restaurant")
         }
-
+        
         return response.json()
     };
 
     const { data:results, isLoading } = useQuery(
-        ["SearchRestaurants"],
+        ["searchRestaurants", searchState],
         createSearchRequest,
         {enabled: !!city}
     );
