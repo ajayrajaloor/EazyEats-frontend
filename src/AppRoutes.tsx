54eb52171d29eb6f37ac76108c8f9fcc1252ddbc
ajayrajaloor <ajayrajaloor@gmail.com>
2024-08-30 20:31:29 +0530

    order status page added also added progress bar in it

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,6 +7,7 @@ import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
 
 const AppRoutes = () => {
 
@@ -27,6 +28,13 @@ const AppRoutes = () => {
       <Route path="/detail/:restaurantId" element={<Layout showBanner={false}> <DetailPage/> </Layout>}/>
 
       <Route element={<ProtectedRoute />}>
+
+      <Route path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage  />
+            </Layout>} />
+
         <Route path="/user-profile"
           element={
             <Layout>
