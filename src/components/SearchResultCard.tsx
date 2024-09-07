dde043af62761f05b208c81f90a662b36648184a
ajayrajaloor <ajayrajaloor@gmail.com>
2024-09-07 19:44:37 +0530

    fixed small issue

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -1,7 +1,7 @@
 import { Restaurant } from "@/types";
 import { Link } from "react-router-dom";
 import { AspectRatio } from "./ui/aspect-ratio";
-import { Banknote, Clock, Dot } from "lucide-react";
+import { Banknote, Dot } from "lucide-react";
 
 type Props = {
   restaurant: Restaurant;
